211280f7e09f2f8a1801236b90e9c4b3214c3c4e
silambarasan <hellosimbu2023@gmail.com>
2024-04-26 20:25:42 +0530

    login

--- routes/home.js
+++ routes/home.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 router.get("/", async (req, res, next) => {
   return res.status(200).json({
-    title: "Express Testing",
+    title: "simbu",
     message: "The app is working properly!",
   });
 });
